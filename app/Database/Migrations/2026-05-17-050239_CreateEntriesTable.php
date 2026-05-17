b882f51654b13b1d3e736326311b65a38198edfe
Gun Gun Priatna <gungunpriatna02@gmail.com>
2026-05-17 12:03:51 +0700

    Create migrations for users and entries tables

---
+++ app/Database/Migrations/2026-05-17-050239_CreateEntriesTable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateEntriesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'content' => [
+                'type' => 'TEXT',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('entries');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('entries');
+    }
+}
